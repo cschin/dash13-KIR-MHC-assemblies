6f56741ba23096ec1eaee79179ea038a9fa9e8a7
Jason Chin <cschin@infoecho.net>
2023-09-14 18:06:55 +0000

    example to use PGR-TK analyzing the fetched DRB1 gene sequence. it will be nice to annotate such view with exons

---
+++ scripts/DRB1-plot.sh
@@ -0,0 +1,5 @@
+pgr-pbundle-decomp --help
+pgr-pbundle-decomp HLA-DRB1_gene.fa HLA-DRB1_gene -w 80 -r 2 --bundle-length-cutoff 24 --min-branch-size 0 --min-cov 0 --min-span 8
+pgr-pbundle-bed2svg HLA-DRB1_gene.bed HLA-DRB1_gene0. --html
+pgr-pbundle-bed2dist HLA-DRB1_gene.bed HLA-DRB1_gene.
+pgr-pbundle-bed2svg HLA-DRB1_gene.bed HLA-DRB1_gene --ddg-file HLA-DRB1_gene.ddg --html
